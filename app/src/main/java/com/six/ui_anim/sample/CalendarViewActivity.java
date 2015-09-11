96e3350a61a9243c56ba5146a558cc6ab7e2b041
hellenxu <heavensxu@gmail.com>
2015-09-11 22:42:48 +0800

    Added: progressWheel&&calendarView

---
+++ app/src/main/java/com/six/ui_anim/sample/CalendarViewActivity.java
@@ -0,0 +1,33 @@
+package com.six.ui_anim.sample;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.widget.Toast;
+
+import com.six.ui_anim.R;
+import com.six.ui_anim.calendar.CalendarView;
+import com.six.ui_anim.calendar.Cell;
+
+/**
+ * @author hzxuxiaolin
+ * @date 2015/9/8
+ * Copyright 2015 NetEase. All rights reserved.
+ */
+
+public class CalendarViewActivity extends Activity implements CalendarView.OnCellTouchListener {
+    private CalendarView cvBrandDiary;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.act_calendar);
+
+        cvBrandDiary = (CalendarView) findViewById(R.id.cv_brand);
+        cvBrandDiary.setOnCellTouchListener(this);
+    }
+
+    @Override
+    public void onTouch(Cell targetCell) {
+        Toast.makeText(CalendarViewActivity.this, "selected: " + targetCell.getDayOfMonth(), Toast.LENGTH_SHORT).show();
+    }
+}
