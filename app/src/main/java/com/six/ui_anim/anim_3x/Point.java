b456b0c453f347dcfa213015f108ed8a97a389fb
hellenxu <heavensxu@gmail.com>
2015-10-15 23:32:49 +0800

    added: object animation demo

---
+++ app/src/main/java/com/six/ui_anim/anim_3x/Point.java
@@ -0,0 +1,24 @@
+package com.six.ui_anim.anim_3x;
+
+/**
+ * @author hellenxu
+ * @date 2015/10/15
+ * Copyright 2015 Six. All rights reserved.
+ */
+public class Point {
+    private float x;
+    private float y;
+
+    public Point(float x, float y){
+        this.x = x;
+        this.y = y;
+    }
+
+    public float getX(){
+        return x;
+    }
+
+    public float getY(){
+        return y;
+    }
+}
