f230c62fe45e68b19d4f79d5407075f4dcc5aa21
hellenxu <heavensxu@gmail.com>
2016-02-14 20:06:26 +0800

    update rich editor 0214

---
+++ app/src/main/java/com/six/ui_anim/richEditor/span/RichPart.java
@@ -0,0 +1,9 @@
+package com.six.ui_anim.richEditor.span;
+
+/**
+ * Created by xxl on 16/2/14.
+ */
+public class RichPart {
+    public int start;
+    public int end;
+}
---
+++ app/src/main/java/com/six/ui_anim/richEditor/span/RichQuoteSpan.java
@@ -0,0 +1,15 @@
+package com.six.ui_anim.richEditor.span;
+
+import android.text.style.QuoteSpan;
+
+/**
+ * Created by xxl on 16/2/14.
+ */
+public class RichQuoteSpan extends QuoteSpan{
+    private static final int DEFAULT_STRIPE_WIDTH = 2;
+    private static final int DEFAULT_GAP_WIDTH = 2;
+    private static final int DEFAULT_COLOR = 0xff0000ff;
+
+
+
+}
