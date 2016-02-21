53120eb8bf4751bc6cab0635ce587b292a5cb9d5
hellenxu <heavensxu@gmail.com>
2016-02-21 20:35:32 +0800

    add: ImageSpan

---
+++ app/src/main/java/com/six/ui_anim/richEditor/span/RichImageSpan.java
@@ -0,0 +1,19 @@
+package com.six.ui_anim.richEditor.span;
+
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.text.style.ImageSpan;
+
+/**
+ * Created by xxl on 16/2/21.
+ */
+
+public class RichImageSpan extends ImageSpan {
+    public RichImageSpan(Context context, Bitmap b) {
+        super(context, b);
+    }
+
+    public RichImageSpan(Context context, Bitmap b, int verticalAlignment) {
+        super(context, b, verticalAlignment);
+    }
+}
