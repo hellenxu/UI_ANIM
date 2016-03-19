046a4398b8ae802cebaa57dbaa2ce6146e564b2d
hellenxu <heavensxu@gmail.com>
2016-03-19 23:23:39 +0800

    xferMode sample.

---
+++ app/src/main/java/com/six/ui_anim/sample/canvas/XferView.java
@@ -0,0 +1,76 @@
+package com.six.ui_anim.sample.canvas;
+
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.Path;
+import android.graphics.PorterDuff;
+import android.graphics.PorterDuffXfermode;
+import android.util.AttributeSet;
+import android.view.MotionEvent;
+import android.view.View;
+
+import com.six.ui_anim.R;
+
+/**
+ * @author hellenxu
+ * @date 16/3/19
+ * Copyright 2016 Six. All rights reserved.
+ */
+public class XferView extends View {
+
+    private Bitmap mBgBitmap, mFgBitmap;
+    private Paint mPaint;
+    private Canvas mCanvas;
+    private Path mPath;
+
+    public XferView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        init();
+    }
+
+    private void init(){
+        mPath = new Path();
+
+        mPaint = new Paint();
+        mPaint.setAlpha(10);
+        mPaint.setStyle(Paint.Style.STROKE);
+        mPaint.setStrokeWidth(50);
+        mPaint.setStrokeJoin(Paint.Join.ROUND);
+        mPaint.setStrokeCap(Paint.Cap.ROUND);
+        mPaint.setXfermode(new PorterDuffXfermode(PorterDuff.Mode.DST_IN));
+
+        mBgBitmap = BitmapFactory.decodeResource(getResources(),
+                R.drawable.bg);
+        mFgBitmap = Bitmap.createBitmap(mBgBitmap.getWidth(), mBgBitmap.getHeight(), Bitmap.Config.ARGB_8888);
+
+        mCanvas = new Canvas(mFgBitmap);
+        mCanvas.drawColor(Color.GRAY);
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        switch (event.getAction()){
+            case MotionEvent.ACTION_DOWN:
+                mPath.reset();
+                mPath.moveTo(event.getX(), event.getY());
+                break;
+            case MotionEvent.ACTION_MOVE:
+                mPath.lineTo(event.getX(), event.getY());
+                break;
+        }
+
+        mCanvas.drawPath(mPath, mPaint);
+        invalidate();
+        return true;
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        canvas.drawBitmap(mBgBitmap, 0, 0, null);
+        canvas.drawBitmap(mFgBitmap, 0, 0, null);
+    }
+}
