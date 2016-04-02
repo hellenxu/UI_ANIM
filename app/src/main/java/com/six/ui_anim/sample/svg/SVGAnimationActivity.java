dafa3b29caf15972efc55f3045f2e83c1daa21d8
hellenxu <heavensxu@gmail.com>
2016-04-02 23:31:45 +0800

    add: a svg animation sample.

---
+++ app/src/main/java/com/six/ui_anim/sample/svg/SVGAnimationActivity.java
@@ -0,0 +1,45 @@
+package com.six.ui_anim.sample.svg;
+
+import android.app.Activity;
+import android.graphics.drawable.Animatable;
+import android.graphics.drawable.Drawable;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+
+import com.six.ui_anim.R;
+
+/**
+ * @author hellenxu
+ * @date 16/4/2
+ * Copyright 2016 Six. All rights reserved.
+ */
+public class SVGAnimationActivity extends Activity implements View.OnClickListener{
+    private ImageView ivSvg;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.act_svg_anim);
+        ivSvg = (ImageView) findViewById(R.id.iv_svg);
+        ivSvg.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.iv_svg:
+                svgAnim();
+                break;
+            default:
+                break;
+        }
+    }
+
+    private void svgAnim(){
+        Drawable svgDrawable = ivSvg.getDrawable();
+        if (svgDrawable instanceof Animatable){
+            ((Animatable) svgDrawable).start();
+        }
+    }
+}
