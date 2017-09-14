b70c29075e1a9bb768b00f4c95f2310d9bc67c16
hellenxu <heavensxu@gmail.com>
2017-09-13 23:44:54 -0400

    Add: ActionSheet2: Decor.

--- app/src/main/java/com/six/ui_anim/action_sheet/ActionSheetDialog.java
+++ app/src/main/java/com/six/ui_anim/action_sheet/ActionSheetDialog.java
@@ -5,9 +5,13 @@ import android.app.Dialog;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.view.Gravity;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.Window;
+import android.view.animation.AnimationUtils;
+import android.view.animation.TranslateAnimation;
 import android.widget.Button;
+import android.widget.FrameLayout;
 
 import com.six.ui_anim.R;
 
@@ -18,6 +22,9 @@ import com.six.ui_anim.R;
  */
 
 public class ActionSheetDialog extends Activity implements View.OnClickListener {
+    private FrameLayout decorView;
+    private View actionSheet;
+    private boolean isRemoveActionSheet;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -38,6 +45,7 @@ public class ActionSheetDialog extends Activity implements View.OnClickListener 
                 showDialog();
                 break;
             case R.id.btn_decor:
+                useDecorView();
                 break;
         }
     }
@@ -51,5 +59,31 @@ public class ActionSheetDialog extends Activity implements View.OnClickListener 
             dialogWindow.setLayout(-1, -2);
         }
         dialog.show();
+    }
+
+    private void useDecorView() {
+        decorView = (FrameLayout) getWindow().getDecorView();
+        int navBarHeight = decorView.getChildAt(1).getHeight();
+        actionSheet = LayoutInflater.from(this).inflate(R.layout.action_sheet, null, false);
+        isRemoveActionSheet = false;
+
+        FrameLayout.LayoutParams params = new FrameLayout.LayoutParams(-1, -2, Gravity.BOTTOM);
+        params.setMargins(0, 0, 0, navBarHeight);
+        actionSheet.setLayoutParams(params);
+        actionSheet.setAnimation(AnimationUtils.loadAnimation(this, R.anim.action_sheet_enter));
+        decorView.addView(actionSheet, decorView.getChildCount());
+    }
+
+    @Override
+    public void onBackPressed() {
+        if(decorView != null) {
+            actionSheet.setAnimation(AnimationUtils.loadAnimation(this, R.anim.action_sheet_exit));
+            decorView.removeView(actionSheet);
+        }
+
+        if(isRemoveActionSheet) {
+            super.onBackPressed();
+        }
+        isRemoveActionSheet = true;
     }
 }
