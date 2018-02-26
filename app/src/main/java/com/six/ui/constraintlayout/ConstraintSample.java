2c6429090fce6135b8c900d97327a8504d85d5e1
hellenxu <heavensxu@gmail.com>
2018-02-25 22:58:39 -0500

    Add: before setting constraint set in java code.

---
+++ app/src/main/java/com/six/ui/constraintlayout/ConstraintSample.java
@@ -0,0 +1,21 @@
+package com.six.ui.constraintlayout;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+
+import com.six.ui.R;
+
+/**
+ * @CopyRight six.ca
+ * Created by Heavens on 2018-02-16.
+ */
+
+public class ConstraintSample extends Activity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.act_constraint_code_sample);
+    }
+}
