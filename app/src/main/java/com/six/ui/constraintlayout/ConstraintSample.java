2f3d9de3a486e2fdca178bfb50dcc652cda0d664
hellenxu <heavensxu@gmail.com>
2018-02-27 21:32:22 -0500

    Add: ConstraintSet connect example.

--- app/src/main/java/com/six/ui/constraintlayout/ConstraintSample.java
+++ app/src/main/java/com/six/ui/constraintlayout/ConstraintSample.java
@@ -7,6 +7,7 @@ import android.support.constraint.ConstraintLayout;
 import android.support.constraint.ConstraintSet;
 import android.transition.TransitionManager;
 import android.view.View;
+import android.widget.Button;
 
 import com.six.ui.R;
 
@@ -19,6 +20,7 @@ public class ConstraintSample extends Activity {
     private ConstraintSet applySet = new ConstraintSet();
     private ConstraintSet centerSet = new ConstraintSet();
     private ConstraintSet resetSet = new ConstraintSet();
+    private ConstraintSet childSet = new ConstraintSet();
     private ConstraintLayout ctlay;
 
     @Override
@@ -60,5 +62,22 @@ public class ConstraintSample extends Activity {
     public void reset(View view) {
         TransitionManager.beginDelayedTransition(ctlay);
         resetSet.applyTo(ctlay);
+    }
+
+    public void onClickButtonA(View view) {
+        TransitionManager.beginDelayedTransition(ctlay);
+
+        childSet.setVisibility(R.id.btn_b, ConstraintSet.GONE);
+        childSet.setVisibility(R.id.btn_c, ConstraintSet.GONE);
+
+        Button btnApply = (Button) findViewById(R.id.btn_apply);
+        childSet.clear(R.id.btn_a);
+        childSet.connect(R.id.btn_a, ConstraintSet.START, R.id.ctlay, ConstraintSet.START, 0);
+        childSet.connect(R.id.btn_a, ConstraintSet.END, R.id.ctlay, ConstraintSet.END, 0);
+        childSet.connect(R.id.btn_a, ConstraintSet.TOP, R.id.ctlay, ConstraintSet.TOP, 0);
+//        childSet.connect(R.id.btn_a, ConstraintSet.BOTTOM, R.id.ctlay, ConstraintSet.BOTTOM, 0);
+        childSet.constrainHeight(R.id.btn_a, (int) btnApply.getY());
+        childSet.constrainWidth(R.id.btn_a, ctlay.getMeasuredWidth());
+        childSet.applyTo(ctlay);
     }
 }
