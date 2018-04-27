fd561b0a733736c4720a1d968a674c4256d7c194
hellenxu <heavensxu@gmail.com>
2018-04-26 22:18:12 -0400

    Modified: use different to cancel touch outside event.

--- app/src/main/java/com/six/ui/tabLayout/SurveyDialog.java
+++ app/src/main/java/com/six/ui/tabLayout/SurveyDialog.java
@@ -17,13 +17,15 @@ public class SurveyDialog extends DialogFragment {
     @NonNull
     @Override
     public Dialog onCreateDialog(Bundle savedInstanceState) {
+        setCancelable(false);
         AlertDialog.Builder builder = new AlertDialog.Builder(getContext(), android.R.style.Theme_DeviceDefault_Light_Dialog_NoActionBar);
         builder.setTitle("Survey")
                 .setMessage("Would you like to give us feedback?")
                 .setPositiveButton("Yes", null)
                 .setNegativeButton("No", null);
         Dialog dialog = builder.create();
-        dialog.setCancelable(false);
+        // not going to be useful, since DialogFragment change behaviours based on this
+//        dialog.setCancelable(false);
         dialog.setCanceledOnTouchOutside(false);
         return dialog;
     }
