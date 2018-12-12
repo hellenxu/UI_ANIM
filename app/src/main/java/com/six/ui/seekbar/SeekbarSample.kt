9703c88d1baded1b4ce492e5e976f0be1f46a752
hellenxu <heavensxu@gmail.com>
2018-12-11 22:15:09 -0500

    Add: add animation to foreground bar.

--- app/src/main/java/com/six/ui/seekbar/SeekbarSample.kt
+++ app/src/main/java/com/six/ui/seekbar/SeekbarSample.kt
@@ -3,6 +3,7 @@ package com.six.ui.seekbar
 import android.app.Activity
 import android.os.Bundle
 import com.six.ui.R
+import kotlinx.android.synthetic.main.act_seekbar.*
 
 /**
  * @CopyRight six.ca
@@ -13,5 +14,15 @@ class SeekbarSample: Activity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.act_seekbar)
+    }
+
+    override fun onResume() {
+        super.onResume()
+        usage.start()
+    }
+
+    override fun onPause() {
+        super.onPause()
+        usage.stop()
     }
 }
--- app/src/main/java/com/six/ui/seekbar/SixSeekbar.kt
+++ app/src/main/java/com/six/ui/seekbar/SixSeekbar.kt
@@ -1,32 +1,69 @@
 package com.six.ui.seekbar
 
+import android.animation.ValueAnimator
 import android.content.Context
 import android.graphics.Canvas
 import android.graphics.Color
 import android.graphics.Paint
+import android.graphics.drawable.Animatable
 import android.util.AttributeSet
 import android.view.View
+import android.view.animation.LinearInterpolator
 
 /**
  * @CopyRight six.ca
  * Created by Heavens on 2018-12-11.
  */
 class SixSeekbar @JvmOverloads constructor(context: Context, attrs: AttributeSet? = null, defStyleAttr: Int = 0
-) : View(context, attrs, defStyleAttr) {
+) : View(context, attrs, defStyleAttr), Animatable {
+
     private val backgroundPaint: Paint = Paint(Paint.ANTI_ALIAS_FLAG)
     private val foregroundPaint: Paint = Paint(Paint.ANTI_ALIAS_FLAG or Paint.DITHER_FLAG)
+    private val loadingAnimator: ValueAnimator
+
+    private var currentUsage: Float = 0f
+    private var currentRight: Float = 0f
 
     init {
+        //paint init
         backgroundPaint.color = Color.LTGRAY
         backgroundPaint.style = Paint.Style.FILL
 
         foregroundPaint.color = Color.BLUE
         foregroundPaint.style = Paint.Style.FILL
+
+        //animation init
+        loadingAnimator = ValueAnimator.ofFloat(0f, 1f)
+        loadingAnimator.interpolator = LinearInterpolator()
+        loadingAnimator.addUpdateListener { animation ->
+            computeProgress(animation.getAnimatedValue() as Float)
+            invalidate()
+        }
+        loadingAnimator.duration = 600
+
+        currentUsage = 400f
+    }
+
+    private fun computeProgress(percentage: Float) {
+        currentRight = percentage * currentUsage
     }
 
     //TODO
     override fun onDraw(canvas: Canvas) {
         canvas.drawRoundRect(110f, 200f, 600f, 200f + 80, 10f,10f, backgroundPaint)
-        canvas.drawRoundRect(110f, 200f, 400f, 200f + 80, 10f,10f, foregroundPaint)
+        canvas.drawRoundRect(110f, 200f, currentRight, 200f + 80, 10f,10f, foregroundPaint)
     }
+
+    override fun isRunning(): Boolean {
+        return loadingAnimator.isRunning
+    }
+
+    override fun start() {
+        loadingAnimator.start()
+    }
+
+    override fun stop() {
+        loadingAnimator.cancel()
+    }
+
 }
