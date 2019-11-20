62945c4e17f87d445e368d5bde796894d6871f8b
hellenxu <heavensxu@gmail.com>
2019-11-19 22:40:01 -0500

    card stack: init.

---
+++ app/src/main/java/com/six/ui/constraintlayout/CardStackSample.kt
@@ -0,0 +1,19 @@
+package com.six.ui.constraintlayout
+
+import android.app.Activity
+import android.os.Bundle
+import com.six.ui.R
+
+/**
+ * @author hellenxu
+ * @date 2019-10-23
+ * Copyright 2019 Six. All rights reserved.
+ */
+class CardStackSample: Activity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        setContentView(R.layout.act_card_stack)
+    }
+}
