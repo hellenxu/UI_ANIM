099e75e64db79a0b7960825116175e1a90c27a86
hellenxu <heavensxu@gmail.com>
2019-11-26 12:38:05 -0500

    custom view: add path drawing with errors.

--- app/src/main/java/com/six/ui/seekbar/SixRubberSeekBar.kt
+++ app/src/main/java/com/six/ui/seekbar/SixRubberSeekBar.kt
@@ -34,6 +34,10 @@ class SixRubberSeekBar @JvmOverloads constructor(private val ctx: Context,
     private var stopX = 0f
     private var stopY = 0f
     private var currentPercentage = .6f
+    private var draggingFinished = true
+    private var thumbX = 0f
+    private var thumbY = 0f
+    private var currentStopX = 0f
 
     override fun onMeasure(widthMeasureSpec: Int, heightMeasureSpec: Int) {
 
@@ -50,15 +54,45 @@ class SixRubberSeekBar @JvmOverloads constructor(private val ctx: Context,
     }
 
     override fun onDraw(canvas: Canvas) {
+        if (draggingFinished) {
+            drawStaticState(canvas)
+        } else {
+            drawDynamicState(canvas)
+        }
+    }
+
+    // TODO
+    private fun drawDynamicState(canvas: Canvas) {
+        // draw the left half part: 3 points; color, strokeWidth
+        val x1 = thumbX / 2
+        val y1 = height / 2f
+        val x2 = x1
+        val y2 = thumbY
+        path.cubicTo(x1, y1, x2, y2, thumbX, thumbY)
+        paint.style = Paint.Style.STROKE
+        paint.color = ContextCompat.getColor(ctx, DEFAULT_PRIMARY_COLOR_ID)
+        paint.strokeWidth = PRIMARY_STROKE_WIDTH
+        canvas.drawPath(path, paint)
+
+        // draw the right half part
+        path.reset()
+        path.cubicTo(thumbX, thumbY, thumbX * 2 - x1, thumbY, thumbX * 2 - x1, y1)
+        paint.color = ContextCompat.getColor(ctx, TRACK_COLOR_ID)
+        paint.strokeWidth = SECONDARY_STROKE_WIDTH
+        canvas.drawPath(path, paint)
+    }
+
+    private fun drawStaticState(canvas: Canvas) {
+        paint.style = Paint.Style.STROKE
         // draw default track
         paint.strokeWidth = SECONDARY_STROKE_WIDTH
-        paint.color = ContextCompat.getColor(ctx,TRACK_COLOR_ID)
+        paint.color = ContextCompat.getColor(ctx, TRACK_COLOR_ID)
         canvas.drawLine(startX, startY, stopX, stopY, paint)
 
         // draw primary track
         paint.strokeWidth = PRIMARY_STROKE_WIDTH
         paint.color = ContextCompat.getColor(ctx, DEFAULT_PRIMARY_COLOR_ID)
-        val currentStopX = stopX * currentPercentage
+        currentStopX = stopX * currentPercentage
         canvas.drawLine(startX, startY, currentStopX, stopY, paint)
 
         // draw thumb
@@ -66,12 +100,41 @@ class SixRubberSeekBar @JvmOverloads constructor(private val ctx: Context,
         paint.strokeWidth = DEFAULT_THUMB_RADIUS
         canvas.translate(currentStopX + DEFAULT_THUMB_RADIUS, 0f)
         canvas.drawCircle(0f, stopY, DEFAULT_THUMB_RADIUS, paint)
-
     }
 
-    // TODO
     override fun onTouchEvent(event: MotionEvent?): Boolean {
+        if (hitThumb(event?.x ?: 0f, event?.y ?: 0f)) {
+            when (event?.action) {
+                MotionEvent.ACTION_DOWN -> {
+                    draggingFinished = true
+                    return true
+                }
+                MotionEvent.ACTION_MOVE -> {
+                    draggingFinished = false
+                    thumbX = event.x
+                    thumbY = event.y
+                    invalidate()
+                    return true
+                }
+                MotionEvent.ACTION_UP, MotionEvent.ACTION_CANCEL -> {
+                    draggingFinished = false
+                    // start animation
+
+                    return true
+
+                }
+            }
+        }
+
         return super.onTouchEvent(event)
+    }
+
+    private fun hitThumb(x: Float, y: Float): Boolean {
+        val leftBound = currentStopX
+        val rightBound = currentStopX + DEFAULT_THUMB_RADIUS * 2
+        val topBound = top + height / 2 - DEFAULT_THUMB_RADIUS
+        val bottomBound = top + height / 2 + DEFAULT_THUMB_RADIUS
+        return x > leftBound && x < rightBound && y > topBound && y < bottomBound
     }
 
     fun setPercentage(percentage: Float) {
