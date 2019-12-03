ce1e485a67c193fbc2821c7ff595a5ce9970ddbe
hellenxu <heavensxu@gmail.com>
2019-12-02 23:22:48 -0500

    rubber: reorganize and dynamic drawing with incorrect coordinates.

--- app/src/main/java/com/six/ui/seekbar/SixRubberSeekBar.kt
+++ app/src/main/java/com/six/ui/seekbar/SixRubberSeekBar.kt
@@ -8,6 +8,9 @@ import android.util.AttributeSet
 import android.view.MotionEvent
 import android.view.View
 import androidx.core.content.ContextCompat
+import androidx.dynamicanimation.animation.FloatValueHolder
+import androidx.dynamicanimation.animation.SpringAnimation
+import androidx.dynamicanimation.animation.SpringForce
 import com.six.ui.R
 
 /**
@@ -23,134 +26,162 @@ class SixRubberSeekBar @JvmOverloads constructor(private val ctx: Context,
         val tmp = Paint()
         tmp.isAntiAlias = true
         tmp.isDither = true
-        tmp.color = ContextCompat.getColor(ctx, DEFAULT_PRIMARY_COLOR_ID)
         tmp.style = Paint.Style.STROKE
-        tmp.strokeWidth = PRIMARY_STROKE_WIDTH
+        tmp.color = ContextCompat.getColor(ctx, DEFAULT_TRACK_COLOR_ID)
         tmp
     }
     private val path = Path()
-    private var startX = 0f
-    private var startY = 0f
-    private var stopX = 0f
-    private var stopY = 0f
-    private var currentPercentage = .6f
-    private var draggingFinished = true
+    private var springAnimation: SpringAnimation? = null
+    private var currentTrackX = 0f
+    private var currentTrackY = 0f
     private var thumbX = 0f
     private var thumbY = 0f
-    private var currentStopX = 0f
+    private var currentPercentage = .6f
+    private var thumbRadius = DEFAULT_THUMB_RADIUS
+    private var draggingFinished = false
 
+    init {
+        getCustomAttributes()
+    }
+
+    // TODO
+    private fun getCustomAttributes() {
+
+    }
+
+    // TODO make sure padding, margin and custom size are working well
     override fun onMeasure(widthMeasureSpec: Int, heightMeasureSpec: Int) {
-
         super.onMeasure(widthMeasureSpec, heightMeasureSpec)
     }
 
     override fun onLayout(changed: Boolean, left: Int, top: Int, right: Int, bottom: Int) {
         super.onLayout(changed, left, top, right, bottom)
-        startX = left.toFloat()
-        startY = (bottom - top) * 0.5f
-        stopX = right.toFloat()
-        stopY = startY
-        println("xxl-startX: $startX; startY: $startY; stopX: $stopX; stopY: $stopY")
+        currentTrackX = width * currentPercentage  + left
+        currentTrackY = (bottom - top) * 0.5f
+        thumbX = currentTrackX
+        thumbY = currentTrackY
     }
 
-    override fun onDraw(canvas: Canvas) {
-        if (draggingFinished) {
-            drawStaticState(canvas)
-        } else {
-            drawDynamicState(canvas)
+    override fun onDraw(canvas: Canvas?) {
+        if (!draggingFinished) {
+            currentTrackX = left + width * currentPercentage
+
+            drawTrack(canvas)
+            drawThumb(canvas)
         }
     }
 
-    // TODO
-    private fun drawDynamicState(canvas: Canvas) {
-        // draw the left half part: 3 points; color, strokeWidth
-        val x1 = thumbX / 2
-        val y1 = height / 2f
-        val x2 = x1
-        val y2 = thumbY
-        path.cubicTo(x1, y1, x2, y2, thumbX, thumbY)
-        paint.style = Paint.Style.STROKE
-        paint.color = ContextCompat.getColor(ctx, DEFAULT_PRIMARY_COLOR_ID)
-        paint.strokeWidth = PRIMARY_STROKE_WIDTH
-        canvas.drawPath(path, paint)
-
-        // draw the right half part
-        path.reset()
-        path.cubicTo(thumbX, thumbY, thumbX * 2 - x1, thumbY, thumbX * 2 - x1, y1)
-        paint.color = ContextCompat.getColor(ctx, TRACK_COLOR_ID)
-        paint.strokeWidth = SECONDARY_STROKE_WIDTH
-        canvas.drawPath(path, paint)
+    private fun drawThumb(canvas: Canvas?) {
+        paint.style = Paint.Style.FILL_AND_STROKE
+        paint.color = ContextCompat.getColor(ctx, HIGHLIGHTED_TRACK_COLOR_ID)
+        canvas?.translate(thumbX, thumbY)
+        canvas?.drawCircle(0f, 0f, thumbRadius, paint)
     }
 
-    private fun drawStaticState(canvas: Canvas) {
+    private fun drawTrack(canvas: Canvas?) {
+        if (thumbY == currentTrackY) {
+            drawStaticState(canvas)
+        } else {
+            drawCubic(canvas)
+        }
+    }
+
+    private fun drawStaticState(canvas: Canvas?) {
+        // first part
         paint.style = Paint.Style.STROKE
-        // draw default track
-        paint.strokeWidth = SECONDARY_STROKE_WIDTH
-        paint.color = ContextCompat.getColor(ctx, TRACK_COLOR_ID)
-        canvas.drawLine(startX, startY, stopX, stopY, paint)
+        paint.strokeWidth = DEFAULT_HIGHLIGHTED_STROKE_WIDTH
+        paint.color = ContextCompat.getColor(ctx, HIGHLIGHTED_TRACK_COLOR_ID)
+        val y = (bottom - top) * 0.5f
+        canvas?.drawLine(left.toFloat(), y, thumbX, y, paint)
+        // second part
+        paint.strokeWidth = DEFAULT_NORMAL_STROKE_WIDTH
+        paint.color = ContextCompat.getColor(ctx, DEFAULT_TRACK_COLOR_ID)
+        canvas?.drawLine(thumbX, y, right.toFloat(), y, paint)
+    }
 
-        // draw primary track
-        paint.strokeWidth = PRIMARY_STROKE_WIDTH
-        paint.color = ContextCompat.getColor(ctx, DEFAULT_PRIMARY_COLOR_ID)
-        currentStopX = stopX * currentPercentage
-        canvas.drawLine(startX, startY, currentStopX, stopY, paint)
+    private fun drawCubic(canvas: Canvas?) {
+        // first part
+        path.reset()
+        paint.color = ContextCompat.getColor(ctx, HIGHLIGHTED_TRACK_COLOR_ID)
+        paint.style = Paint.Style.STROKE
+        paint.strokeWidth = DEFAULT_HIGHLIGHTED_STROKE_WIDTH
+        val xOffSet = (currentTrackX - left) / 2
+        val x1 = left + xOffSet
+        val y1 = (top + height / 2).toFloat()
+        val x2 = x1
+        val y2 = thumbY
+        println("xxl-first: x1 = $x1; y1 = $y1; x2 = $x2; y2 = $y2; thumbX = $thumbX; thumbY = $thumbY")
+        path.cubicTo(x1, y1, x2, y2, thumbX, thumbY)
+        canvas?.drawPath(path, paint)
 
-        // draw thumb
-        paint.style = Paint.Style.FILL_AND_STROKE
-        paint.strokeWidth = DEFAULT_THUMB_RADIUS
-        canvas.translate(currentStopX + DEFAULT_THUMB_RADIUS, 0f)
-        canvas.drawCircle(0f, stopY, DEFAULT_THUMB_RADIUS, paint)
+        // second part
+        path.reset()
+        paint.color = ContextCompat.getColor(ctx, DEFAULT_TRACK_COLOR_ID)
+        paint.strokeWidth = DEFAULT_NORMAL_STROKE_WIDTH
+        println("xxl-second: x1 = $thumbX; y1 = $thumbY; x2 = ${thumbX + xOffSet}; y2 = $thumbY; thumbX = ${thumbX + xOffSet}; thumbY = $y1")
+        path.cubicTo(thumbX, thumbY, thumbX + xOffSet, thumbY, thumbX + xOffSet, y1)
+        canvas?.drawPath(path, paint)
     }
 
     override fun onTouchEvent(event: MotionEvent?): Boolean {
-        if (hitThumb(event?.x ?: 0f, event?.y ?: 0f)) {
-            println("xxl00")
-            when (event?.action) {
+        if (event != null && hitThumb(event.x, event.y)) {
+            when (event.action) {
                 MotionEvent.ACTION_DOWN -> {
-                    draggingFinished = true
+                    draggingFinished = false
+                    currentTrackX = event.x
+                    currentTrackY = event.y
+                    invalidate()
                     return true
                 }
                 MotionEvent.ACTION_MOVE -> {
                     draggingFinished = false
+                    springAnimation?.cancel()
                     thumbX = event.x
                     thumbY = event.y
                     invalidate()
                     return true
                 }
-                MotionEvent.ACTION_UP, MotionEvent.ACTION_CANCEL -> {
-                    draggingFinished = false
-                    // start animation
-
+                MotionEvent.ACTION_UP,
+                MotionEvent.ACTION_CANCEL -> {
+                    draggingFinished = true
+                    springAnimation = SpringAnimation(FloatValueHolder(currentTrackY))
+                        .setStartValue(thumbY)
+                        .setSpring(
+                            SpringForce(currentTrackY)
+                                .setDampingRatio(SpringForce.DAMPING_RATIO_LOW_BOUNCY)
+                                .setStiffness(SpringForce.STIFFNESS_LOW)
+                        )
+                    springAnimation?.start()
                     return true
-
                 }
             }
         }
-        println("xxl11")
 
         return super.onTouchEvent(event)
     }
 
     private fun hitThumb(x: Float, y: Float): Boolean {
-        val leftBound = currentStopX
-        val rightBound = currentStopX + DEFAULT_THUMB_RADIUS * 2
-        val topBound = top + height / 2 - DEFAULT_THUMB_RADIUS * 1.1
-        val bottomBound = top + height / 2 + DEFAULT_THUMB_RADIUS * 1.1
-        println("xxl-x: $x; y: $y")
-        println("xxl-left: $leftBound; right: $rightBound; top: $topBound; bottom: $bottomBound")
+        val leftBound = (thumbX - thumbRadius).coerceAtLeast(left.toFloat())
+        val rightBound = (thumbX + thumbRadius).coerceAtMost(right.toFloat())
+        val topBound = (top + height / 2 - thumbRadius).coerceAtLeast(top.toFloat())
+        val bottomBound = (bottom - height / 2 + thumbRadius).coerceAtMost(bottom.toFloat())
         return x > leftBound && x < rightBound && y > topBound && y < bottomBound
     }
 
-    fun setPercentage(percentage: Float) {
-        currentPercentage = percentage.coerceAtLeast(0f).coerceAtMost(1f)
+    fun setCurrentPercentage(percentage: Float) {
+        currentPercentage = percentage
+    }
+
+    fun setThumbRadius(radius: Float) {
+        thumbRadius = radius.coerceAtLeast(0f)
     }
 
     companion object {
-        private const val TRACK_COLOR_ID = R.color.grey_font
-        private const val DEFAULT_PRIMARY_COLOR_ID = R.color.blue_500
-        private const val SECONDARY_STROKE_WIDTH = 8F
-        private const val PRIMARY_STROKE_WIDTH = 12F
-        private const val DEFAULT_THUMB_RADIUS = 20F
+        private const val DEFAULT_TRACK_COLOR_ID = R.color.grey_font
+        private const val HIGHLIGHTED_TRACK_COLOR_ID = R.color.blue_200
+        private const val DEFAULT_THUMB_RADIUS = 20f
+        private const val DEFAULT_HIGHLIGHTED_STROKE_WIDTH = 15f
+        private const val DEFAULT_NORMAL_STROKE_WIDTH = 10f
     }
 
 }
