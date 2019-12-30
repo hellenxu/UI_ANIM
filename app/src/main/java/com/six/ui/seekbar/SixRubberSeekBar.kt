03648f24d3d05a0e1d2815c04fdad6efdcc4d22b
hellenxu <heavensxu@gmail.com>
2019-12-30 18:51:14 -0500

    fix: onTouchEvent an onDraw.

--- app/src/main/java/com/six/ui/seekbar/SixRubberSeekBar.kt
+++ app/src/main/java/com/six/ui/seekbar/SixRubberSeekBar.kt
@@ -5,13 +5,13 @@ import android.graphics.Canvas
 import android.graphics.Paint
 import android.graphics.Path
 import android.util.AttributeSet
+import android.util.TypedValue
 import android.view.MotionEvent
 import android.view.View
 import androidx.core.content.ContextCompat
-import androidx.dynamicanimation.animation.FloatValueHolder
 import androidx.dynamicanimation.animation.SpringAnimation
-import androidx.dynamicanimation.animation.SpringForce
 import com.six.ui.R
+import kotlin.math.max
 
 /**
  * @author hellenxu
@@ -31,6 +31,7 @@ class SixRubberSeekBar @JvmOverloads constructor(private val ctx: Context,
         tmp
     }
     private val path = Path()
+    private val minHeight = TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, 100f, ctx.resources.displayMetrics)
     private var springAnimation: SpringAnimation? = null
     private var trackY = 0f
     private var trackX = 0f
@@ -51,24 +52,36 @@ class SixRubberSeekBar @JvmOverloads constructor(private val ctx: Context,
 
     // TODO make sure padding, margin and custom size are working well
     override fun onMeasure(widthMeasureSpec: Int, heightMeasureSpec: Int) {
-        super.onMeasure(widthMeasureSpec, heightMeasureSpec)
+        val height = MeasureSpec.getSize(heightMeasureSpec)
+        val heightMode = MeasureSpec.getMode(heightMeasureSpec)
+        var realHeight = 0
+
+        when (heightMode) {
+            MeasureSpec.AT_MOST -> {
+                realHeight = minHeight.toInt()
+            }
+
+            MeasureSpec.EXACTLY,
+            MeasureSpec.UNSPECIFIED -> {
+                realHeight = max(minHeight.toInt(), height)
+            }
+
+        }
+
+        setMeasuredDimension(widthMeasureSpec, MeasureSpec.makeMeasureSpec(realHeight, heightMode))
     }
 
     override fun onLayout(changed: Boolean, left: Int, top: Int, right: Int, bottom: Int) {
         super.onLayout(changed, left, top, right, bottom)
-        trackX = width * currentPercentage  + left
+        trackX = width * currentPercentage + left
         trackY = (bottom - top) * 0.5f
         thumbX = trackX
         thumbY = trackY
     }
 
     override fun onDraw(canvas: Canvas?) {
-        if (!draggingFinished) {
-            trackX = left + width * currentPercentage
-
-            drawTrack(canvas)
-            drawThumb(canvas)
-        }
+        drawTrack(canvas)
+        drawThumb(canvas)
     }
 
     private fun drawThumb(canvas: Canvas?) {
@@ -106,11 +119,10 @@ class SixRubberSeekBar @JvmOverloads constructor(private val ctx: Context,
         paint.style = Paint.Style.STROKE
         paint.strokeWidth = DEFAULT_HIGHLIGHTED_STROKE_WIDTH
         val xOffSet = (trackX - left) / 2
-        val x1 = left + xOffSet
-        val y1 = (top + height / 2).toFloat()
-        val x2 = (x1 + trackX) / 2
-        val y2 = y1 + height / 2
-        println("xxl-first: x1 = $x1; y1 = $y1; x2 = $x2; y2 = $y2; thumbX = $thumbX; thumbY = $thumbY")
+        val x1 = (left + trackX) / 2
+        val y1 = height.toFloat() / 2
+        val x2 = x1
+        val y2 = thumbY
         path.moveTo(left.toFloat(), trackY)
         path.cubicTo(x1, y1, x2, y2, thumbX, thumbY)
         canvas?.drawPath(path, paint)
@@ -119,54 +131,78 @@ class SixRubberSeekBar @JvmOverloads constructor(private val ctx: Context,
         path.reset()
         paint.color = ContextCompat.getColor(ctx, DEFAULT_TRACK_COLOR_ID)
         paint.strokeWidth = DEFAULT_NORMAL_STROKE_WIDTH
-        println("xxl-second: x1 = $thumbX; y1 = $thumbY; x2 = ${thumbX + xOffSet}; y2 = $thumbY; thumbX = ${thumbX + xOffSet}; thumbY = $y1")
-        path.moveTo(right.toFloat(), trackY)
-        path.cubicTo(right - xOffSet, y1, thumbX + xOffSet, y2, thumbX, thumbY)
+        path.moveTo(thumbX, thumbY)
+        path.cubicTo(thumbX + xOffSet, y2, right - xOffSet, y1, right.toFloat() - thumbRadius, trackY)
         canvas?.drawPath(path, paint)
 
     }
 
     override fun onTouchEvent(event: MotionEvent?): Boolean {
-        if (event != null && hitThumb(event.x, event.y)) {
-            when (event.action) {
-                MotionEvent.ACTION_DOWN -> {
+        when (event?.action) {
+            MotionEvent.ACTION_DOWN, MotionEvent.ACTION_MOVE -> {
+                if (event.action == MotionEvent.ACTION_DOWN && hitThumb(event.x, event.y)) {
                     draggingFinished = false
-                    trackX = event.x
-                    invalidate()
-                    return true
                 }
-                MotionEvent.ACTION_MOVE -> {
-                    draggingFinished = false
-                    springAnimation?.cancel()
+
+                return if (!draggingFinished) {
+                    trackX = event.x
                     thumbX = event.x
                     thumbY = event.y
                     invalidate()
-                    return true
+                    true
+                } else {
+                    false
                 }
-                MotionEvent.ACTION_UP,
-                MotionEvent.ACTION_CANCEL -> {
-                    draggingFinished = true
-                    springAnimation = SpringAnimation(FloatValueHolder(trackY))
-                        .setStartValue(thumbY)
-                        .setSpring(
-                            SpringForce(trackY)
-                                .setDampingRatio(SpringForce.DAMPING_RATIO_LOW_BOUNCY)
-                                .setStiffness(SpringForce.STIFFNESS_LOW)
-                        )
-                    springAnimation?.start()
-                    return true
-                }
+            }
+            MotionEvent.ACTION_UP -> {
+                draggingFinished = true
+                invalidate()
+                return true
+            }
+            else -> {
+                return super.onTouchEvent(event)
             }
         }
 
-        return super.onTouchEvent(event)
+//        if (event != null && hitThumb(event.x, event.y)) {
+//            when (event.action) {
+//                MotionEvent.ACTION_DOWN -> {
+//                    draggingFinished = false
+//                    trackX = event.x
+//                    invalidate()
+//                    return true
+//                }
+//                MotionEvent.ACTION_MOVE -> {
+//                    draggingFinished = false
+//                    springAnimation?.cancel()
+//                    thumbX = event.x
+//                    thumbY = event.y
+//                    invalidate()
+//                    return true
+//                }
+//                MotionEvent.ACTION_UP,
+//                MotionEvent.ACTION_CANCEL -> {
+//                    draggingFinished = true
+////                    springAnimation = SpringAnimation(FloatValueHolder(trackY))
+////                        .setStartValue(thumbY)
+////                        .setSpring(
+////                            SpringForce(trackY)
+////                                .setDampingRatio(SpringForce.DAMPING_RATIO_LOW_BOUNCY)
+////                                .setStiffness(SpringForce.STIFFNESS_LOW)
+////                        )
+////                    springAnimation?.start()
+//                    return true
+//                }
+//            }
+//        }
     }
 
     private fun hitThumb(x: Float, y: Float): Boolean {
-        val leftBound = (thumbX - thumbRadius).coerceAtLeast(left.toFloat())
-        val rightBound = (thumbX + thumbRadius).coerceAtMost(right.toFloat())
-        val topBound = (top + height / 2 - thumbRadius).coerceAtLeast(top.toFloat())
-        val bottomBound = (bottom - height / 2 + thumbRadius).coerceAtMost(bottom.toFloat())
+        val offset = 1.2
+        val leftBound = thumbX - thumbRadius * offset
+        val rightBound = thumbX + thumbRadius * offset
+        val topBound = thumbY - thumbRadius * offset
+        val bottomBound = thumbY + thumbRadius * offset
         return x > leftBound && x < rightBound && y > topBound && y < bottomBound
     }
 
