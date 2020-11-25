b1f9295d2406712b616ce474f695481c418838b8
hellenxu <heavensxu@gmail.com>
2020-11-25 13:27:48 -0500

    mask tab init: drew two layers.

---
+++ app/src/main/java/com/six/ui/xmode/SampleActivity.kt
@@ -0,0 +1,12 @@
+package com.six.ui.xmode
+
+import androidx.appcompat.app.AppCompatActivity
+import com.six.ui.R
+
+/**
+ * @author hellenxu
+ * @date 2020-11-25
+ * Copyright 2020 Six. All rights reserved.
+ */
+class SampleActivity: AppCompatActivity(R.layout.act_mask_tab) {
+}
